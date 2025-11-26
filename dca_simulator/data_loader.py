5694c83cd40b2d5a6c82d8d61bddf415f5d5d541
Maxim Milde <73267075@fsv.cuni.cz>
2025-11-26 21:20:56 +0100

    fixed the dca_DD functiongit add .

--- dca_simulator/data_loader.py
+++ dca_simulator/data_loader.py
@@ -5,4 +5,10 @@ def load_price_data(ticker: str, start_date: str, end_date: str):
     """Download daily adj. close prices for a stock""" 
         #yfinance does not have an 'Adj Close' column anymore 
     df = yf.download(ticker, start=start_date, end=end_date)
-    return df[["Close"]] #'Close' is already adjusted for div. and stock splits
+
+    if isinstance(df.columns, pd.MultiIndex):
+        df = df.droplevel(1, axis=1) #drop Ticker
+        
+
+    df = df[["Close"]]
+    return df #'Close' is already adjusted for div. and stock splits
--- dca_simulator/strategies.py
+++ dca_simulator/strategies.py
@@ -1,37 +1,41 @@
 import pandas as pd
 
-def dca_DD(df: pd.DataFrame, monthly_contrib: float, DD_treshold: float=0.2):
-    """Double Down DCA when price is 20% below the rolling 1-year high"""
+def dca_DD(df: pd.DataFrame, monthly_contrib: float, DD_threshold: float=0.2):
+    """Double Down DCA: Investing monthly_contrib, 
+    unless the price is 20% below the rolling 1-year high,
+    then invest 2x monthly_contrib"""
 
     df = df.copy()
+
     df["12m_high"] = df["Close"].rolling(252).max() #252 trading days in a year
-    df["DD_cond"] = df["Close"] <= (1-DD_treshold) * df["12m_high"]
+    df["drawdown"] = df["Close"]/df["12m_high"]
+    df["DD_cond"] = df["drawdown"] <= (1-DD_threshold) #This is True when the stock is >=20% down from 12m_high
 
-    monthly_price = df.resample("M").first() #because if we specify an exact date manually it could be a non-trading day
+    df = df.dropna(subset=["12m_high"]).copy() #because the 1y rolling high will be NaN for the first year
+
+    monthly_investments = df.resample("MS").first() #because if we specify an exact date manually it could be a non-trading day
     #we buy at the first trading day of each month
+    monthly_investments["DD_cond"] = (monthly_investments['Close']/monthly_investments['12m_high'] <= (1-DD_threshold))
 
-    shares = []
-    invested = []
+
     shares_total = 0
     invested_total = 0
 
-    for date, row in monthly_price.iterrows():
-        price = row["Close"]
-
-        investment_amount = monthly_contrib * (2 if row["DD_cond"] else 1)
+    for date, row in monthly_investments.iterrows():
+        multiplier = 2 if row["DD_cond"] else 1
+        investment_amount = monthly_contrib * multiplier
         #doubling the contribution every time the price drops 20% from rolling high
 
-        shares_bought = investment_amount/price
+        shares_bought = investment_amount/row["Close"]
         shares_total += shares_bought
         invested_total += investment_amount
 
-        shares.append(shares_total)
-        invested.append(invested_total)
+        monthly_investments.loc[date, 'shares_total'] = shares_total
+        monthly_investments.loc[date, 'invested_total'] = invested_total
+        monthly_investments.loc[date, 'portf_value'] = shares_total * row['Close']
 
-    result = monthly_price.copy()
-    result["shares_total"] = shares #putting the results into the df monthly_price
-    result["invested_total"] = invested
-    result["portf_value"] = result["shares_total"]*result["Close"]
-    result["profit_loss"] = result["porf_value"] - result["invested_total"]
-    return result
+    monthly_investments['profit_loss'] = monthly_investments['portf_value'] - monthly_investments['invested_total']
+    return monthly_investments
+
+    
 
